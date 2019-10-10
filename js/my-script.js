e0de73b37c8ab23f705aa12f302d7be2466f7ed0
brillientgirl170 <brillientgirl170@gmail.com>
2019-10-10 18:23:21 +0600

    create image gallery by jquery

---
+++ js/my-script.js
@@ -0,0 +1,140 @@
+
+$('#img1').click(function () {
+    var subImageValue= $('#img1').attr('src');
+    $('#mainImage').attr('src', subImageValue);
+});
+
+
+
+
+// var inputElement = document.getElementById('firstName');
+// inputElement.onkeyup = function(){
+//     var firstNameValue = document.getElementById('firstName').value;
+//     document.getElementById('F1').innerHTML = firstNameValue;
+// }
+// var inputElement= document.getElementById('lastName');
+// inputElement.onkeyup= function(){
+//     var firstNameValue= document.getElementById('lastName').value;
+//     document.getElementById('F2').innerHTML= firstNameValue;
+// }
+// var inputElement= document.getElementById('lastName');
+// inputElement.onblur= function(){
+//     var firstNameValue= document.getElementById('firstName').value;
+//     var lastNameValue= document.getElementById('lastName').value;
+//     var fullName= (firstNameValue+" "+lastNameValue);
+//     document.getElementById('F3').innerHTML= fullName;
+// }
+
+
+//
+// var btnElement= document.getElementById('redBtn');
+// btnElement.onclick= function(){
+//     var redBtnValue= document.getElementById('div');
+//         redBtnValue.style.backgroundColor= 'red';
+// }
+// var btnElement= document.getElementById('greenBtn');
+// btnElement.onclick= function () {
+//     var greenBtnValue= document.getElementById('div');
+//         greenBtnValue.style.backgroundColor= 'green';
+// }
+// var btnElement= document.getElementById('yelloBtn');
+// btnElement.onclick= function () {
+//     var yelloBtnValue= document.getElementById('div');
+//         yelloBtnValue.style.backgroundColor= 'yellow';
+// }
+// var btnElement= document.getElementById('defaultBtn');
+// btnElement.onclick= function () {
+//     var defaultBtnValue= document.getElementById('div');
+//         defaultBtnValue.style.backgroundColor= 'lightgray';
+// }
+
+
+
+
+
+
+
+// $('#firstName').keyup(function () {
+//     var firstNameValue= $('#firstName').val();
+//         $('#F1').text(firstNameValue);
+// });
+// $('#lastName').keyup(function () {
+//     var lastNameValue= $('#lastName').val();
+//         $('#F2').html(lastNameValue);
+// });
+// $('#lastName').blur(function () {
+//     var firstNameValue= $('#firstName').val();
+//     var firstNameValue= $('#lastName').val();
+//         $('#F3').text(firstNameValue+" "+firstNameValue);
+// });
+
+
+
+
+
+// var x= "abcdefjhbf";
+// var y= x.length;
+// document.write(y);
+//
+// var x= "abcdefjhbf";
+// var y= x.indexOf('x');
+// document.write(y);
+
+
+// var x= "we are \"Vikings\" ";
+// alert(x);/
+
+
+
+ 
+
+
+// function createHeadingElement(){
+// var newInputElement = document.createElement('input');
+// var newParagraphElement = document.createElement('p');
+//
+// newInputElement.value = 'This is Heading.';
+// newParagraphElement.innerHTML = 'This is Paragraph.';
+//
+// document.getElementById('div').appendChild(newInputElement);
+// document.getElementById('div').appendChild(newParagraphElement);
+//
+// };
+//
+// document.getElementById('btn').onclick = function(){
+//     createHeadingElement();
+// };
+
+
+
+// $('#firstName').keyup(function () {
+//      var firstNameValue= $('#firstName').val();
+//         $('#res1').text(firstNameValue);
+// });
+// $('#lastName').keyup(function () {
+//     var lastNameValue= $('#lastName').val();
+//     $('#res2').text(lastNameValue);
+// });
+// $('#lastName').blur(function () {
+//     var firstNameValue= $('#firstName').val();
+//     var lastNameValue= $('#lastName').val();
+//     $('#res3').text(firstNameValue+" "+lastNameValue);
+// });
+
+
+
+// alert(document.getElementById('h1').innerHTML);
+// alert(document.getElementById('input').value);
+// alert(document.getElementById('img').getAttribute('src'));
+//
+// document.getElementById('h1').innerHTML= 'this is not heading';
+// document.getElementById('input').value='this is not input';
+// document.getElementById('img').setAttribute('src', 'images/Femaledoctor.png');
+//
+
+
+
+
+
+
+
